bfe97b30593f40704b22c88e60e3470b0e154cdd
Sven Marquardt <Sven2q@hotmail.de>
2015-11-16 23:59:21 +0100

    Logik hinter thread verstanden

--- Aufgabenblatt3/Aufgabe3_1/main.c
+++ Aufgabenblatt3/Aufgabe3_1/main.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+int variable=2;
+pthread_t threads[2];
+
+void* showThread(void *args) {
+
+    if(pthread_equal(pthread_self(),threads[0])){
+        printf("Hello from the first thread\n");
+    }else {
+        printf("Hello from second thread\n");
+    }
+
+    return NULL;
+
+}
 
 int main(void)
 {
-    printf("Hello World!\n");
-    return 0;
+
+
+    int error;
+    int i=0;
+    pthread_attr_t attr;
+
+    while (i<2) {
+        error=pthread_create(&(threads[i]),NULL,&showThread,NULL);
+        if (error!=0) {
+            printf("Error, can't create thread\n");
+        }else {
+            printf("Thread created!\n");
+        }
+        i=i+1;
+
+    }
+
+    sleep(5);
+
+
+
 }
 
